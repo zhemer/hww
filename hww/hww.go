cbe4a9f383ca48c7cfc88403c362c0d579bcb220
Sergey Zhemerdeev <zhemers@gmail.com>
2019-07-09 12:19:41 +0300

    Source re-format

--- hww/hww.go
+++ hww/hww.go
@@ -1,34 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net/http"
+	"io/ioutil"
 	"log"
-	"strings"
-	"strconv"
+	"net/http"
 	"os"
-	"regexp"
 	"os/exec"
-    "io/ioutil"
-    "flag"
-    "time"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 )
-
 
 type tCpuData [7]int
 
 const (
-    sUrlRoot = "/"
-    sUrlVars = "/varz"
-    sUrlHealth = "/healthz"
-    sUrlStatus = "/statusz"
-    sUrlHealthInvert = "/healthzInvert"
+	sUrlRoot         = "/"
+	sUrlVars         = "/varz"
+	sUrlHealth       = "/healthz"
+	sUrlStatus       = "/statusz"
+	sUrlHealthInvert = "/healthzInvert"
 )
 
 var (
-	sParPort        = flag.String("listen-port", "8080", "The port to listen on for HTTP requests.")
-	iParRefresh	= flag.Int("refresh-interval", 30, "Interval for front page refreshes (0=disable)")
-	iParRequestLog	= flag.Int("request-log", 1, "Log request to console (0=disable)")
+	sParPort       = flag.String("listen-port", "8080", "The port to listen on for HTTP requests.")
+	iParRefresh    = flag.Int("refresh-interval", 30, "Interval for front page refreshes (0=disable)")
+	iParRequestLog = flag.Int("request-log", 1, "Log request to console (0=disable)")
 )
 
 var stCpu tCpuData
@@ -49,58 +48,57 @@ var sHelp = `Application end points:
 func main() {
 	tiStarted = time.Now()
 	flag.Usage = func() {
-        fmt.Fprintf(flag.CommandLine.Output(), "Simple http server that exposes CPU usage to world\n")
-        fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
-        flag.PrintDefaults()
-        fmt.Fprintf(flag.CommandLine.Output(), "\n" + sHelp)
+		fmt.Fprintf(flag.CommandLine.Output(), "Simple http server that exposes CPU usage to world\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintf(flag.CommandLine.Output(), "\n"+sHelp)
 	}
 	flag.Parse()
 	if *iParRefresh > 0 {
-		pageTop = fmt.Sprintf(pageTop, "<meta http-equiv=refresh content=" + strconv.Itoa(*iParRefresh) +">")
+		pageTop = fmt.Sprintf(pageTop, "<meta http-equiv=refresh content="+strconv.Itoa(*iParRefresh)+">")
 	}
 	go getCpuLoad()
 
 	http.HandleFunc(sUrlHealth, func(w http.ResponseWriter, r *http.Request) {
 		iHdr, sStr := 200, "ok"
 		RequestLog(r)
-	    if ! iStatHealth {
-	    	iHdr, sStr = 500, "error"
-	    }
-        w.WriteHeader(iHdr)
-        w.Write([]byte(sStr))
+		if !iStatHealth {
+			iHdr, sStr = 500, "error"
+		}
+		w.WriteHeader(iHdr)
+		w.Write([]byte(sStr))
 	})
 	http.HandleFunc(sUrlHealthInvert, func(w http.ResponseWriter, r *http.Request) {
 		RequestLog(r)
-	    iStatHealth = ! iStatHealth
-	    w.Write([]byte(fmt.Sprintf("Health status changed from %v to %v", !iStatHealth, iStatHealth)))
+		iStatHealth = !iStatHealth
+		w.Write([]byte(fmt.Sprintf("Health status changed from %v to %v", !iStatHealth, iStatHealth)))
 	})
 
 	http.HandleFunc(sUrlStatus, func(w http.ResponseWriter, r *http.Request) {
 		iHdr, sStr := 200, "ready"
 		RequestLog(r)
-	    if ! iStatReady {
-	    	iHdr, sStr = 500, "not ready"
-	    }
-        w.WriteHeader(iHdr)
-        w.Write([]byte(sStr))
+		if !iStatReady {
+			iHdr, sStr = 500, "not ready"
+		}
+		w.WriteHeader(iHdr)
+		w.Write([]byte(sStr))
 	})
 
 	http.HandleFunc(sUrlVars, func(w http.ResponseWriter, r *http.Request) {
 		tUp := time.Now().Unix() - tiStarted.Unix()
-        w.WriteHeader(200)
-        w.Write([]byte(fmt.Sprintf("hww_user %d\nhww_nice %d\nhww_system %d\nhww_idle %d\nhww_iowait %d\nhww_uptime %d\nhww_health %v\nhww_ready %v\n",
-        	stCpu[0],stCpu[1],stCpu[2],stCpu[3],stCpu[4], tUp, Bool2Int(iStatHealth), Bool2Int(iStatReady) )))
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("hww_user %d\nhww_nice %d\nhww_system %d\nhww_idle %d\nhww_iowait %d\nhww_uptime %d\nhww_health %v\nhww_ready %v\n",
+			stCpu[0], stCpu[1], stCpu[2], stCpu[3], stCpu[4], tUp, Bool2Int(iStatHealth), Bool2Int(iStatReady))))
 	})
 
 	fmt.Printf("Listening on port %s\n", *sParPort)
 	http.HandleFunc(sUrlRoot, PageIndex)
 	iStatReady = true
-	if err := http.ListenAndServe(":"+ *sParPort, nil); err != nil {
+	if err := http.ListenAndServe(":"+*sParPort, nil); err != nil {
 		log.Fatal("Failed to start server", err)
 		iStatHealth, iStatReady = false, false
 	}
 }
-
 
 func RunCommand(sCmd string) string {
 	sOut, err := exec.Command(sCmd).Output()
@@ -111,26 +109,23 @@ func RunCommand(sCmd string) string {
 	return string(sOut)
 }
 
-
 func RequestLog(request *http.Request) {
 	if *iParRequestLog == 1 {
 		fmt.Printf("%v %v %v\n", time.Now(), request.RemoteAddr, request.URL.Path)
 	}
 }
 
-
 func PageIndex(writer http.ResponseWriter, request *http.Request) {
 	RequestLog(request)
 
 	fmt.Fprint(writer, pageTop)
-	for _, s := range(saUrls) {
+	for _, s := range saUrls {
 		fmt.Fprintf(writer, "| <a href=\"%s\">%s</a> ", s, s)
 	}
 	sPage := fmt.Sprintf("date:<br>%s\nuptime:<br>%s\nps:<br>%s\n", RunCommand("date"), RunCommand("uptime"), RunCommand("ps"))
 	fmt.Fprintf(writer, "<pre>%s</pre>", sPage)
 	fmt.Fprint(writer, pageBottom)
 }
-
 
 func getCpuData() tCpuData {
 	rawBytes, err := ioutil.ReadFile("/proc/stat")
@@ -147,13 +142,12 @@ func getCpuData() tCpuData {
 	s := reSpace.ReplaceAllString(sLine, " ")
 
 	as := strings.Split(s, " ")
-	for i,v := range as[1:8] {
-		aiCpuData[i],err = strconv.Atoi(v)
+	for i, v := range as[1:8] {
+		aiCpuData[i], err = strconv.Atoi(v)
 		aiCpuData[i] /= iCpu
 	}
 	return aiCpuData
 }
-
 
 func getCpuLoad() {
 	for {
@@ -161,16 +155,15 @@ func getCpuLoad() {
 		time.Sleep(time.Second)
 		data1 := getCpuData()
 
-		for i,v := range data1 {
+		for i, v := range data1 {
 			stCpu[i] = v - data0[i]
 		}
 	}
 }
 
-
 func Bool2Int(b bool) int {
 	if b {
-	    return 1
+		return 1
 	}
 	return 0
 }
