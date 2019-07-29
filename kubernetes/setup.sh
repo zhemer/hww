f832bea16e1d0d478e8dbb052465bc17b78bcdcd
Sergey Zhemerdeev <zhemers@gmail.com>
2019-07-29 07:53:04 +0300

    Labels for all objects are added

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -1,8 +1,26 @@
-apps='hww prometheus grafana'
-for app in $apps;do
-  echo kubectl apply -f "${app}-service.yaml"
-done
-for app in $apps;do
-  echo kubectl apply -f "${app}-deployment.yaml"
-done
+if [ "$1" != '-i' ] && [ "$1" != '-u' ];then
+    cat << EOF
+$0 installs services and deployments on Kubernetes cluster.
+Actually, the script just outputs commands to stdout, you need to apped '|sh +x' to command line to execute them.
+Usage: $0 -i|-u
+-i 		installs services and deployments
+-u 		remove them
+EOF
+    exit
+fi
 
+case "$1" in
+-i):
+	apps='hww prometheus grafana'
+	for app in $apps;do
+		echo kubectl apply -f "${app}-service.yaml"
+	done
+	for app in $apps;do
+		echo kubectl apply -f "${app}-deployment.yaml"
+	done
+;;
+
+-u)
+	echo "kubectl delete deployments,services,configmap -l 'app in (hww,grafana,prometheus),owner=zhemer'"
+;;
+esac
